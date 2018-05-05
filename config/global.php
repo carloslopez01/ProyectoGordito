03caccddb4c61fdf65c1dcb97757babaeb13243a
Carlos Lopez <cl640900@gmail.com>
2018-05-05 09:21:56 -0600

    Aplicando DAO

---
+++ config/global.php
@@ -0,0 +1,5 @@
+<?php
+// definiendo constantes de acciones default
+define("CONTROLADOR_DEFECTO", "Usuarios");
+define("ACCION_DEFECTO", "index");
+?>
---
+++ php/login.php
@@ -0,0 +1,13 @@
+<?php 
+	//asignando las variables un valor recibido desde el index principal
+ 	$usuario= $_POST['usuario'];
+ 	$clave = $_POST['clave'];
+
+ 	//verificando que las variables no esten vacias.
+ 	if (isset($usuario) && isset($clave)) {
+ 		
+ 	}else{
+ 		
+ 	}
+
+ ?>
